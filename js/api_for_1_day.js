7d5d1871ea4672fff22ab058af9b2884792ee398
Andriy1years <144339667+Andriy1years@users.noreply.github.com>
2024-05-18 21:59:29 +0300

    ff

--- js/api_for_1_day.js
+++ js/api_for_1_day.js
@@ -2,6 +2,30 @@ const send = document.getElementById('send');
 const search = document.getElementById('search');
 const error = document.getElementById('error');
 const register_menu = document.getElementById('register_menu');
+const misto = document.getElementById('misto');
+const degree = document.getElementById('degree');
+const day = document.getElementById('day');
+const text = document.getElementById('text');
+const max_degree = document.getElementById('max_degree');
+const min_degree = document.getElementById('min_degree');
+const rain = document.getElementById('rain');
+const up_p = document.getElementById('up_p');
+const down_p = document.getElementById('down_p')
+
+//week
+const DayElement = document.getElementById("nuul_day");
+const DayDegreeElement = document.getElementById("day_degree");
+const oneDayElement = document.getElementById("one_day");
+const oneDayDegreeElement = document.getElementById("one_day_degree");
+const twoDayElement = document.getElementById("two_day");
+const twoDayDegreeElement = document.getElementById("two_day_degree");
+const threeDayElement = document.getElementById("three_day");
+const threeDayDegreeElement = document.getElementById("three_day_degree");
+const fourDayElement = document.getElementById("four_day");
+const fourDayDegreeElement = document.getElementById("four_day_degree");
+const fiveDayElement = document.getElementById("five_day");
+const fiveDayDegreeElement = document.getElementById("five_day_degree");
+let forecast_number = 0
 
 send.addEventListener("click", () => {
     const apiKey = '7f616499082a1f88bd07737fbf9768fd';
@@ -11,11 +35,14 @@ send.addEventListener("click", () => {
 
 search.addEventListener("keypress", (event) => {
     if (event.key === "Enter") {
-        const apiKey = '7f616499082a1f88bd07737fbf9768fd';
-        const city = search.value;
-        getWeatherForecast(apiKey, city);
+
     }
 });
+
+const apiKey = '7f616499082a1f88bd07737fbf9768fd';
+//const city = search.value;
+const city = "хуст"
+getWeatherForecast(apiKey, city);
 
 function getWeatherForecast(apiKey, city) {
     const url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
@@ -40,6 +67,13 @@ function getWeatherForecast(apiKey, city) {
 
             // Прогноз на завтра
             const forecast = data.list[0];
+            const forecast1 = data.list[1];
+            const forecast2 = data.list[2];
+            const forecast3 = data.list[3];
+            const forecast4 = data.list[4];
+            const forecast5 = data.list[5];
+
+
 
             const temperature = forecast.main.temp;
             const rainProbability = forecast.pop * 100;
@@ -52,7 +86,7 @@ function getWeatherForecast(apiKey, city) {
             const sunset = new Date(data.city.sunset * 1000);
 
             const daysOfWeek = ['Неділя', 'Понеділок', 'Вівторок', 'Середа', 'Четвер', 'П\'ятниця', 'Субота'];
-            let dayIndex = new Date().getDay() + 1;
+            let dayIndex = new Date().getDay() + 0;
             if (dayIndex >= daysOfWeek.length) {
                 dayIndex = 0;
             }
@@ -68,6 +102,26 @@ function getWeatherForecast(apiKey, city) {
             console.log(`Час заходу сонця: ${sunset.toLocaleTimeString()}`);
             console.log(`Ймовірність опадів: ${rainProbability.toFixed(2)}%`);
             register_menu.style.display = "none";
+            //city
+            misto.innerHTML = city;
+            //day
+            day.innerHTML = dayOfWeek;
+            DayElement.innerHTML = dayOfWeek;
+            oneDayElement.innerHTML = daysOfWeek[(dayIndex + 1) % daysOfWeek.length];
+            twoDayElement.innerHTML = daysOfWeek[(dayIndex + 2) % daysOfWeek.length];
+            threeDayElement.innerHTML = daysOfWeek[(dayIndex + 3) % daysOfWeek.length];
+            fourDayElement.innerHTML = daysOfWeek[(dayIndex + 4) % daysOfWeek.length];
+            fiveDayElement.innerHTML = daysOfWeek[(dayIndex + 5) % daysOfWeek.length];
+            //temperature
+            DayDegreeElement.innerHTML = Math.round(forecast.main.temp) + "°C";
+            degree.innerHTML = Math.round(forecast.main.temp) + "°C";
+            oneDayDegreeElement.innerHTML = Math.round(forecast1.main.temp) + "°C";
+            twoDayDegreeElement.innerHTML = Math.round(forecast2.main.temp) + "°C";
+            threeDayDegreeElement.innerHTML = Math.round(forecast3.main.temp) + "°C";
+            fourDayDegreeElement.innerHTML = Math.round(forecast4.main.temp) + "°C";
+            fiveDayDegreeElement.innerHTML = Math.round(forecast5.main.temp) + "°C";
+            //translatedDescription
+            text.innerHTML = translatedDescription;
         })
         .catch(error => {
             console.error(`Сталася помилка: ${error.message}`);
