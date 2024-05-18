4b7430348f6bd57a631128c3822ae0c8f20835f4
Andriy1years <144339667+Andriy1years@users.noreply.github.com>
2024-05-19 00:01:25 +0300

    Update api_for_1_day.js

--- js/api_for_1_day.js
+++ js/api_for_1_day.js
@@ -53,7 +53,8 @@ search.addEventListener("keypress", (event) => {
 
 
 function getWeatherForecast(apiKey, city) {
-    const url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric
+    `;
 
     fetch(url)
         .then(response => {
